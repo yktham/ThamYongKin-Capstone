692128cfbee3000c52b1fc1c25274610a5c407e0
yktham <yongkintham@gmail.com>
2026-02-07 12:47:36 +0800

    Update vite.config.js

--- stockform/my-react-app/vite.config.js
+++ stockform/my-react-app/vite.config.js
@@ -5,9 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/ThamYongKin-Capstone/',
-  // test: {
-  //   globals: true,
-  //   environment: 'jsdom',
-  //   setupFiles: 'src/setupTests.js',
-  // },
 })
