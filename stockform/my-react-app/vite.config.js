0f1132595e4efa06b98d03ff911b8810b4bdefa3
yktham <yongkintham@gmail.com>
2026-02-07 12:45:06 +0800

    Update vite.config.js

--- stockform/my-react-app/vite.config.js
+++ stockform/my-react-app/vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/THAMYONGKIN-CAPSTONE/stockform/my-react-app/',
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: 'src/setupTests.js',
-  },
+  base: '/ThamYongKin-Capstone/',
+  // test: {
+  //   globals: true,
+  //   environment: 'jsdom',
+  //   setupFiles: 'src/setupTests.js',
+  // },
 })
